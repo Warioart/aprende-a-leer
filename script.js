8c4abd58ebebb8970decb1716584ad24a104d20d
Warioart <wario1313@gmail.com>
2025-06-02 23:08:40 -0400

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 
 const questions = [
-  { word: "manzana", image: "https://i.pinimg.com/736x/1f/96/be/1f96be83ffd0c7f1cd54cc25c7aad1c5.jpg", options: ["manzana", "pera"] },
-  { word: "gato", image: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg", options: ["perro", "gato"] },
-  { word: "avion", image: "https://upload.wikimedia.org/wikipedia/commons/3/34/Airplane_silhouette.png", options: ["barco", "avion"] },
-  { word: "perro", image: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Collage_of_Nine_Dogs.jpg", options: ["gato", "perro"] },
-  { word: "flor", image: "https://upload.wikimedia.org/wikipedia/commons/5/50/Flower_poster_2.jpg", options: ["flor", "zapato"] }
+{ word: "fresa", image: "https://upload.wikimedia.org/wikipedia/commons/2/29/PerfectStrawberry.jpg", options: ["fresa", "sandía"] },
+  { word: "sandía", image: "https://upload.wikimedia.org/wikipedia/commons/e/e4/Watermelon_cross_BNC.jpg", options: ["melón", "sandía"] },
+  { word: "piña", image: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Pineapple_and_cross_section.jpg", options: ["piña", "limón"] },
+  { word: "limón", image: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Lemon-Whole-Split.jpg", options: ["naranja", "limón"] },
+  { word: "naranja", image: "https://upload.wikimedia.org/wikipedia/commons/c/c4/Orange-Fruit-Pieces.jpg", options: ["naranja", "mandarina"] },
+  { word: "zapato", image: "https://upload.wikimedia.org/wikipedia/commons/4/4c/Black_Shoes.jpg", options: ["zapato", "sombrero"] },
+  { word: "sombrero", image: "https://upload.wikimedia.org/wikipedia/commons/6/65/Hat_on_hook.jpg", options: ["sombrero", "guante"] },
+  { word: "guante", image: "https://upload.wikimedia.org/wikipedia/commons/2/29/Gloves_icon.png", options: ["guante", "calcetín"] },
+  { word: "calcetín", image: "https://upload.wikimedia.org/wikipedia/commons/8/84/Socks.jpg", options: ["camisa", "calcetín"] },
+  { word: "camisa", image: "https://upload.wikimedia.org/wikipedia/commons/3/3e/White_Shirt.jpg", options: ["camisa", "pantalón"] },
+  { word: "pantalón", image: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Jeans_for_women.jpg", options: ["falda", "pantalón"] },
+  { word: "falda", image: "https://upload.wikimedia.org/wikipedia/commons/1/1f/Skirt_2020.jpg", options: ["vestido", "falda"] },
+  { word: "vestido", image: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Dress_in_pink.jpg", options: ["vestido", "abrigo"] },
+  { word: "abrigo", image: "https://upload.wikimedia.org/wikipedia/commons/f/fb/Winter_coat_black.jpg", options: ["abrigo", "bufanda"] },
+  { word: "bufanda", image: "https://upload.wikimedia.org/wikipedia/commons/4/45/Woollen_scarf.jpg", options: ["bufanda", "gorra"] },
+  { word: "gorra", image: "https://upload.wikimedia.org/wikipedia/commons/a/a3/Baseball_cap.jpg", options: ["sombrero", "gorra"] },
+  { word: "vaso", image: "https://upload.wikimedia.org/wikipedia/commons/4/45/Glass_water.jpg", options: ["vaso", "botella"] },
+  { word: "botella", image: "https://upload.wikimedia.org/wikipedia/commons/2/25/Water_bottle.jpg", options: ["botella", "caja"] },
+  { word: "caja", image: "https://upload.wikimedia.org/wikipedia/commons/f/f3/Cardboard_box.jpg", options: ["caja", "bolsa"] },
+  { word: "bolsa", image: "https://upload.wikimedia.org/wikipedia/commons/f/fd/Shopping_bag.jpg", options: ["bolsa", "silla"] },
+  { word: "silla", image: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Wooden_chair.jpg", options: ["mesa", "silla"] },
+  { word: "mesa", image: "https://upload.wikimedia.org/wikipedia/commons/5/54/Table.jpg", options: ["cama", "mesa"] },
+  { word: "cama", image: "https://upload.wikimedia.org/wikipedia/commons/d/dc/Modern_bed.jpg", options: ["cama", "sofá"] },
+  { word: "sofá", image: "https://upload.wikimedia.org/wikipedia/commons/d/d0/Red_Sofa.jpg", options: ["silla", "sofá"] },
+  { word: "reloj", image: "https://upload.wikimedia.org/wikipedia/commons/4/4e/Wristwatch.jpg", options: ["reloj", "cuadro"] },
+  { word: "cuadro", image: "https://upload.wikimedia.org/wikipedia/commons/f/fc/Painting_icon.png", options: ["cuadro", "lámpara"] },
+  { word: "lámpara", image: "https://upload.wikimedia.org/wikipedia/commons/6/60/Lamp_icon.png", options: ["televisión", "lámpara"] },
+  { word: "televisión", image: "https://upload.wikimedia.org/wikipedia/commons/5/53/TV_icon.png", options: ["radio", "televisión"] },
+  { word: "radio", image: "https://upload.wikimedia.org/wikipedia/commons/5/5e/Radio_icon_2.png", options: ["teléfono", "radio"] },
+  { word: "teléfono", image: "https://upload.wikimedia.org/wikipedia/commons/5/55/Smartphone_icon.png", options: ["teléfono", "ratón"] }
 ];
 
 let current = 0;
